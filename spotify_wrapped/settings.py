cfb4141717aadf56f3bae79240fba2c5eef33341
tracyyxie <127050708+tracyyxie@users.noreply.github.com>
2025-01-06 11:59:02 -0500

    key fix

--- spotify_wrapped/settings.py
+++ spotify_wrapped/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0a#e0+yghxs3cpb-*6s00x0g^f&u!l8)fogf_&ey01&4m@)4&x"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
